9561a9def622ae823f2ffb0a62b579882ae6c7c0
Yanis2002 <70938875+Yanis2002@users.noreply.github.com>
2025-12-01 19:18:56 +0300

    Fix all apostrophes in music file paths - use double quotes
    - Changed all music file paths with apostrophes to use double quotes
    - This prevents syntax errors when file names contain apostrophes
    - Functions addAudioTrack, addPhoto, addVideo now use safe methods (textContent, setAttribute)

--- docs/script.js
+++ docs/script.js
@@ -1624,8 +1624,9 @@ function loadLocalMusic() {
         console.log('Загрузка музыки, найден элемент:', audioList);
         
         // Список всех локальных аудио файлов
+        // Используем двойные кавычки для путей с апострофами, чтобы избежать ошибок
         const localMusic = [
-        { src: 'music/Abel Korzeniowski - Evgeni\'s Waltz.mp3', title: 'Abel Korzeniowski Evgeni\'s Waltz', duration: '0:00' },
+            { src: "music/Abel Korzeniowski - Evgeni's Waltz.mp3", title: "Abel Korzeniowski Evgeni's Waltz", duration: '0:00' },
         { src: 'music/Adam Ferello - Infinity.mp3', title: 'Adam Ferello Infinity', duration: '0:00' },
         { src: 'music/Assasin`s Cred - из Асасинс Крид 2.mp3', title: 'Assasin`s Cred из Асасинс Крид 2', duration: '0:00' },
         { src: 'music/Ben Howard - Oats In The Water.mp3', title: 'Ben Howard Oats In The Water', duration: '0:00' },
@@ -1636,7 +1637,7 @@ function loadLocalMusic() {
         { src: 'music/Calvin Harris - My Way.mp3', title: 'Calvin Harris My Way', duration: '0:00' },
         { src: 'music/Clair De Lune - The Evil Within - 2014 Soundtrack OST.mp3', title: 'Clair De Lune The Evil Within 2014 Soundtrack OST', duration: '0:00' },
         { src: 'music/Clint Mansell - Lux Aeterna (OST Requiem for a Dream) - Вечный свет (ОСТ Реквием по мечте) оригинальная.mp3', title: 'Clint Mansell Lux Aeterna (OST Requiem for a Dream) Вечный свет (ОСТ Реквием по мечте) оригинальная', duration: '0:00' },
-        { src: 'music/Clint Mansell - Robbo\'s Theme.mp3', title: 'Clint Mansell Robbo\'s Theme', duration: '0:00' },
+        { src: "music/Clint Mansell - Robbo's Theme.mp3", title: "Clint Mansell Robbo's Theme", duration: '0:00' },
         { src: 'music/Daft Punk - Instant Crush.mp3', title: 'Daft Punk Instant Crush', duration: '0:00' },
         { src: 'music/Dvar - ariil iaat.mp3', title: 'Dvar ariil iaat', duration: '0:00' },
         { src: 'music/Erik Satie - Gymnopedia №1.mp3', title: 'Erik Satie Gymnopedia №1', duration: '0:00' },
